600aa0957fa04359405599c0253f343330c120df
PRANSHU PATEL <141228888+PatelPranshu@users.noreply.github.com>
2023-09-01 09:40:19 +0530

    Delete timer.js

--- timer.js
+++
@@ -1,9 +0,0 @@
-setTimeout(function(){
-    window.location.href = 'css page.html';
-    var executed = false;
-    return function() {
-        if (!executed) {
-            executed = true;
-        }
-    };
- }, 3000);
